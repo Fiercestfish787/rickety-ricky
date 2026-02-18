406e3a0d82b664d2495bd27db8f77123cbc3921a
Fiercestfish787 <chris_gibson2000@hotmail.com>
2026-02-18 17:11:31 +0000

    I changed it so that Ricky can memorise you when you click logo.

--- main.ts
+++ main.ts
@@ -8,9 +8,9 @@ input.onButtonPressed(Button.AB, function () {
 radio.onReceivedString(function (receivedString) {
     basic.showString(receivedString)
     if (receivedString.length == 24) {
-        datalogger.log(datalogger.createCV("Name", receivedString.substr(11, 12)))
+        datalogger.log(datalogger.createCV("Name", receivedString.substr(6, 12)))
     } else {
-        datalogger.log(datalogger.createCV("Name", receivedString.substr(11, 11)))
+        datalogger.log(datalogger.createCV("Name", receivedString.substr(6, 11)))
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -57,13 +57,56 @@ input.onGesture(Gesture.Shake, function () {
         . . . . .
         `)
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString("Log:")
+    while (!(input.logoIsPressed())) {
+        if (input.buttonIsPressed(Button.A)) {
+            if (Letter == 27) {
+                datalogger.log(datalogger.createCV("Name", Log))
+            } else if (Letter == 28) {
+                Log = Log.substr(0, Log.length - 1)
+            } else if (Letter == 29) {
+                if (Alphabet == "abcdefghijklmnopqrstuvwxyz") {
+                    Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                } else {
+                    Alphabet = "abcdefghijklmnopqrstuvwxyz"
+                }
+            } else {
+                Log = "" + Log + Alphabet.substr(Letter, Letter)
+            }
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            if (Letter == 26) {
+                Letter += 1
+                basic.showString("Done?")
+            } else if (Letter == 27) {
+                Letter += 1
+                basic.showString("Back?")
+            } else if (Letter == 28) {
+                Letter += 1
+                basic.showString("Caps lock?")
+            } else if (Letter == 29) {
+                Letter = 1
+                basic.showString(Alphabet.substr(Letter, Letter))
+            } else {
+                Letter += 1
+                basic.showString(Alphabet.substr(Letter, Letter))
+            }
+        }
+    }
+    basic.showString("Successfully logged.")
+})
 let Randomise = 0
+let Log = ""
+let Letter = 0
 let Name = ""
+let Alphabet = ""
+Alphabet = "abcdefghijklmnopqrstuvwxyz"
 datalogger.setColumnTitles("Name")
 radio.setGroup(1)
 radio.setTransmitPower(7)
 Name = "Ricky" + randint(1, 50)
-let Number2 = 1
+Letter = 1
 basic.showLeds(`
     # # . # #
     # # . # #
@@ -100,5 +143,9 @@ basic.forever(function () {
             servos.P0.run(-35)
             servos.P1.run(35)
         }
+    }
+    Randomise = randint(1, 3)
+    if (Randomise == 3) {
+        basic.pause(200)
     }
 })
